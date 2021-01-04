9f9c44f6783fb94cdd09a08da5f99290c31ac002
Kegen Guyll <guyllkegen@gmail.com>
2021-01-03 22:38:08 -0600

    updated pre-push

--- src/services/joke/DeleteJoke.service.ts
+++ src/services/joke/DeleteJoke.service.ts
@@ -4,9 +4,7 @@ import { mongoService } from '../..';
 export const deleteJokeService = async (id: string) => {
   const objectid = new ObjectId(id);
   try {
-    const result = await mongoService.db('Jokes')
-    .collection('DadJokes')
-    .deleteOne({ _id: objectid });
+    const result = await mongoService.db('Jokes').collection('DadJokes').deleteOne({ _id: objectid });
     return { success: true, body: result };
   } catch (err) {
     return { success: false, error: err };
