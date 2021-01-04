acf8ae3e4b0ef9a0025d8cdee9e8fee237fe00f4
Kegen Guyll <guyllkegen@gmail.com>
2021-01-03 22:20:26 -0600

    added routes for new controllers

--- src/routes/joke.ts
+++ src/routes/joke.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { CountJokeController } from '../controllers/joke/CountJoke.controller';
+import { CreateJokeController } from '../controllers/joke/CreateJoke.controller';
+import { DeleteJokeController } from '../controllers/joke/DeleteJoke.controller';
 import { JokeByIdController } from '../controllers/joke/JokeById.controller';
 import { JokeBySearchController } from '../controllers/joke/JokeBySearch.controller';
 import { JokeByTypeController } from '../controllers/joke/JokeByType.controller';
@@ -6,10 +9,16 @@ import wrapAsync from '../middleware/async.middleware';
 
 const router: Router = Router();
 
+router.get('/count', wrapAsync(CountJokeController));
+
 router.get('/search', wrapAsync(JokeBySearchController));
 
 router.get('/:id', wrapAsync(JokeByIdController));
 
 router.get('/type/:type', wrapAsync(JokeByTypeController));
+
+router.post('/create', wrapAsync(CreateJokeController));
+
+router.delete('/delete', wrapAsync(DeleteJokeController));
 
 export const JokeRouter: Router = router;
