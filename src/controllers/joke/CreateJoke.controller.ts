16617a89d45bc2d69a39354f13c15d786ae1c9fe
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:30:09 -0500

    requried token to create joke

--- src/controllers/joke/CreateJoke.controller.ts
+++ src/controllers/joke/CreateJoke.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import { createJokeService } from '../../services/joke/CreateJoke.service';
+import sessionToken from '../../util/getSessionToken';
 
 export const createJokeController = async (req: Request, res: Response) => {
   try {
-    const createdJoke = await createJokeService(req.body);
+    const createdJoke = await createJokeService(sessionToken(req), req.body);
     res.send(createdJoke);
   } catch (err) {
     res.status(500).send(err);
