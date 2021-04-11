bf0d30b2ac4066157445bd3536c8e2f8afde2fc2
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:32:06 -0500

    added get and post like routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import { createUserController } from '../controllers/user/CreateUser.controller';
 import { loginUserController } from '../controllers/user/LoginUser.controller';
 import { profileUserController } from '../controllers/user/ProfileUser.controller';
+import {
+  CreateLikeJokeController,
+  ListLikeJokeController,
+} from '../controllers/user/LikeJoke.controller';
 import asyncMiddleware from '../middleware/async.middleware';
 
 const router: Router = Router();
@@ -11,5 +15,9 @@ router.post('/register', asyncMiddleware(createUserController));
 router.post('/login', asyncMiddleware(loginUserController));
 
 router.get('/profile', asyncMiddleware(profileUserController));
+
+router.get('/like', asyncMiddleware(ListLikeJokeController));
+
+router.post('/like/:jokeId', asyncMiddleware(CreateLikeJokeController));
 
 export const userRouter: Router = router;
