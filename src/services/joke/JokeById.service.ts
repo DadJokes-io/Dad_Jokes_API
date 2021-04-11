7b604aa3073ea4d6deb926604c79e947e0e19fe9
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:33:24 -0500

    updated type

--- src/services/joke/JokeById.service.ts
+++ src/services/joke/JokeById.service.ts
@@ -1,10 +1,14 @@
 import { ObjectId } from 'mongodb';
 import { mongoService } from '../..';
+import { Joke } from '../../models/joke';
 
 export const jokeByIdService = async (id: string) => {
   try {
     const oId = new ObjectId(id);
-    const result = await mongoService.db('Jokes').collection('DadJokes').findOne({ _id: oId });
+    const result = (await mongoService
+      .db('Jokes')
+      .collection('DadJokes')
+      .findOne({ _id: oId })) as Joke;
     return { success: true, body: result };
   } catch (err) {
     return { success: false, error: err };
