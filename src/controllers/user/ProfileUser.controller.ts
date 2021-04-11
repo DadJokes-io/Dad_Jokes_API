9baccfc86b5548075ed12dc52c397e32a5667ab1
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:31:16 -0500

    updated to user sessionToken util

--- src/controllers/user/ProfileUser.controller.ts
+++ src/controllers/user/ProfileUser.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 import { profileUserService } from '../../services/user/ProfileUser.service';
+import sessionToken from '../../util/getSessionToken';
 
 export const profileUserController = async (req: Request, res: Response) => {
   try {
-    const sessionToken = req.headers.authorization?.split(' ')[1];
-
-    const userProfile = await profileUserService(sessionToken);
+    const userProfile = await profileUserService(sessionToken(req));
     res.send(userProfile);
   } catch (err) {
     res.status(500).send(err);
