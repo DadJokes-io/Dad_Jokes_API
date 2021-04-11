11564da64429735f8edb2559595fcfeb89448a36
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:30:22 -0500

    required token to delete joke

--- src/controllers/joke/DeleteJoke.controller.ts
+++ src/controllers/joke/DeleteJoke.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import { deleteJokeService } from '../../services/joke/DeleteJoke.service';
+import sessionToken from '../../util/getSessionToken';
 
 export const deleteJokeController = async (req: Request, res: Response) => {
   try {
-    const removedJoke = await deleteJokeService(req.params.id);
+    const removedJoke = await deleteJokeService(sessionToken(req), req.params.id);
     res.send(removedJoke);
   } catch (err) {
     res.status(500).send(err);
