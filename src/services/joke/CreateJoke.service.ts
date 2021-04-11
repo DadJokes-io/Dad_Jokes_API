690def9d95675adbfaefd3be71e6be48f32304d7
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:32:43 -0500

    added new fields to joke

--- src/services/joke/CreateJoke.service.ts
+++ src/services/joke/CreateJoke.service.ts
@@ -1,15 +1,36 @@
 import { mongoService } from '../..';
+import { CreateJokeBody } from '../../models/joke';
+import oID from '../../util/oID';
+import { authUser } from '../user/authUser.service';
 
-interface Body {
-  type: string;
-  setup: string;
-  punchline: string;
-}
-
-export const createJokeService = async (body: Body) => {
+export const createJokeService = async (sessionToken: string | undefined, body: CreateJokeBody) => {
   try {
-    const result = await mongoService.db('Jokes').collection('DadJokes').insertOne(body);
-    return { success: true, body: result };
+    const user = await authUser(sessionToken);
+
+    if (user instanceof Error) throw user;
+
+    const result = await mongoService
+      .db('Jokes')
+      .collection('DadJokes')
+      .insertOne({
+        ...body,
+        author: {
+          name: user.displayName,
+          id: oID(user._id),
+        },
+        date: Math.floor(new Date().getTime() / 1000),
+        approved: false,
+      });
+
+    const jokeId = result.ops[0]._id as string;
+    user.posts.push(jokeId);
+
+    await mongoService
+      .db('Users')
+      .collection('Profile')
+      .updateOne({ _id: oID(user._id) }, { $set: { posts: user.posts } });
+
+    return { success: true, body: result.ops[0] };
   } catch (err) {
     return { success: false, error: err };
   }
