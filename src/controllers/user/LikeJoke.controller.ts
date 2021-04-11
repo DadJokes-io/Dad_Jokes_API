3d8d5cc7be00733f224267dc11adee671df78700
Kegen Guyll <guyllkegen@gmail.com>
2021-04-11 13:30:40 -0500

    created like joke controller

---
+++ src/controllers/user/LikeJoke.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { authUser } from '../../services/user/authUser.service';
+import { LikeJokeService } from '../../services/user/LikeJoke.service';
+import sessionToken from '../../util/getSessionToken';
+
+export const CreateLikeJokeController = async (req: Request, res: Response) => {
+  try {
+    const jokeId = req.params.jokeId as string;
+
+    const likeJokeResult = await LikeJokeService(sessionToken(req), jokeId);
+    res.send(likeJokeResult);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const ListLikeJokeController = async (req: Request, res: Response) => {
+  try {
+    const likeJokeResult = await authUser(sessionToken(req));
+
+    if (likeJokeResult instanceof Error) throw likeJokeResult;
+
+    res.send(likeJokeResult.likes);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
