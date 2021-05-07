a04d3577569646ac6e8bc221a981b825d8b3b94c
Kegen Guyll <guyllkegen@gmail.com>
2021-05-07 14:40:06 -0500

    imageKit initialization

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-require('dotenv').config();
+import 'dotenv';
 import mongodb from 'mongodb';
+import ImageKit from 'imagekit';
+
 const port = process.env.PORT || 8080;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 const host = process.env.DB_HOST;
 
+const publicKey = process.env.IMAGEKIT_PUBLIC_KEY as string;
+const privateKey = process.env.IMAGEKIT_PRIVATE_KEY as string;
+const urlEndpoint = process.env.IMAGEKIT_URLENDPOINT as string;
+
 const MongoClient = mongodb.MongoClient;
 const uri = `mongodb+srv://${user}:${password}@${host}`;
 //const uri = `mongodb://localhost:27017`;
+
+export const imagekit = new ImageKit({
+  publicKey,
+  privateKey,
+  urlEndpoint,
+});
+
 export const mongoService = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
