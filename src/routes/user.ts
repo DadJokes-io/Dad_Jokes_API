bc4bfc8c73865863ee86f6156433dd0fd1ea74b1
Kegen Guyll <guyllkegen@gmail.com>
2021-05-07 14:30:44 -0500

    added new routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,6 +8,10 @@ import {
 } from '../controllers/user/LikeJoke.controller';
 import asyncMiddleware from '../middleware/async.middleware';
 import { publicUserController } from '../controllers/user/PublicUser.controller';
+import { getUserPostController } from '../controllers/user/GetUserPosts.controller';
+import { getUserLikesController } from '../controllers/user/GetUserLikes.controller';
+import { updateUserController } from '../controllers/user/UpdateUser.controller';
+import { UnlikeJokeController } from '../controllers/user/UnlikeJoke.controller';
 
 const router: Router = Router();
 
@@ -21,6 +25,14 @@ router.get('/like', asyncMiddleware(ListLikeJokeController));
 
 router.get('/public/profile/:id', asyncMiddleware(publicUserController));
 
+router.put('/unlike/:jokeId', asyncMiddleware(UnlikeJokeController));
+
 router.post('/like/:jokeId', asyncMiddleware(CreateLikeJokeController));
+
+router.get('/likes/:userId', asyncMiddleware(getUserLikesController));
+
+router.get('/posts/:userId', asyncMiddleware(getUserPostController));
+
+router.put('/', asyncMiddleware(updateUserController));
 
 export const userRouter: Router = router;
