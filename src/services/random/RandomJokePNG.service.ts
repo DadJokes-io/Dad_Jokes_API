59d6bc9b545206704330bb470b5554e3dd4209ee
Kegen Guyll <guyllkegen@gmail.com>
2021-07-05 12:07:32 -0500

    created new route for PNG

---
+++ src/services/random/RandomJokePNG.service.ts
@@ -0,0 +1,41 @@
+import { mongoService } from '../..';
+import { Joke } from '../../model/joke';
+const text2png = require('text2png');
+
+export const randomJokePNGService = async () => {
+  try {
+    const joke: Array<Joke> = await mongoService
+      .db('Jokes')
+      .collection('DadJokes')
+      .aggregate([{ $sample: { size: 1 } }])
+      .toArray();
+
+    let customItemsPunchline = joke[0].punchline.split(',');
+
+    for (let i = 0; i < customItemsPunchline.length; i++) {
+      customItemsPunchline[i] + '\n';
+    }
+
+    joke[0].punchline = customItemsPunchline.join('');
+
+    let customItemsSetup = joke[0].setup.split(',');
+
+    for (let i = 0; i < customItemsSetup.length; i++) {
+      customItemsSetup[i] + '\n';
+    }
+
+    joke[0].setup = customItemsSetup.join('');
+
+    const png = text2png(`${joke[0].setup} \n \n${joke[0].punchline}`, {
+      color: 'black',
+      lineSpacing: 10,
+      padding: 20,
+      output: 'dataURL',
+    });
+
+    return { success: true, body: png };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
