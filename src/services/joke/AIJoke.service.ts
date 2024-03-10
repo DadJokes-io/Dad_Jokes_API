14da9b0ed4b0403cad87d457b66a2030d6074fa7
Kegen Guyll <guyllkegen@gmail.com>
2024-03-09 21:20:57 -0600

    Comment out OpenAI imports and usage

--- src/services/joke/AIJoke.service.ts
+++ src/services/joke/AIJoke.service.ts
@@ -1,37 +1,39 @@
-import OpenAI from 'openai';
-import { openai } from '../..';
+// import OpenAI from 'openai';
+// import { openai } from '../..';
 
 export const AiJokeService = async (jokeTopic: string) => {
   try {
-    const params: OpenAI.Chat.ChatCompletionCreateParams = {
-      messages: [
-        { 
-          role: 'user', 
-          content: `imagine you are an experienced comedian tasked with creating a joke with a given topic; ${jokeTopic}. In a JSON formatted response separating the setup and punchline`
-        }
-      ],
-      max_tokens: 50,
-      response_format: { "type": "json_object" },
-      model: 'gpt-3.5-turbo-1106',
-    };
-    const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+    // const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    //   messages: [
+    //     { 
+    //       role: 'user', 
+    //       content: `imagine you are an experienced comedian tasked with creating a joke with a given topic; ${jokeTopic}. In a JSON formatted response separating the setup and punchline`
+    //     }
+    //   ],
+    //   max_tokens: 50,
+    //   response_format: { "type": "json_object" },
+    //   model: 'gpt-3.5-turbo-1106',
+    // };
+    // const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
 
-    const jokes = []
+    // const jokes = []
 
-    for(let i = 0; i < chatCompletion.choices.length; i++) {
-      const messageContent = chatCompletion.choices[i].message.content
+    // for(let i = 0; i < chatCompletion.choices.length; i++) {
+    //   const messageContent = chatCompletion.choices[i].message.content
 
-      if(messageContent){
-        jokes.push({
-          ...JSON.parse(messageContent)
-        })
-      }
-    }
+    //   if(messageContent){
+    //     jokes.push({
+    //       ...JSON.parse(messageContent)
+    //     })
+    //   }
+    // }
 
-    return { 
-      success: true, 
-      body: jokes
-    };
+    // return { 
+    //   success: true, 
+    //   body: jokes
+    // };
+
+    return 'ok'
   } catch (err) {
     return { success: false, error: err };
   }
